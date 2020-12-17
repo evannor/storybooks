2d5b8e1fbbaaab02c499bb43f122760597bae7f7
evannor <erica.vannorman@gmail.com>
2020-12-16 19:18:16 -0800

    Create routes and views for templating

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 
 // Load config
@@ -10,9 +11,17 @@ connectDB();
 
 const app = express();
 
+// Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// Routes
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 3000;
 
