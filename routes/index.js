656a9f12172ae3437acda9a62062151c2ef8abaa
evannor <erica.vannorman@gmail.com>
2021-01-04 20:37:41 -0800

    Table and setup for Dashboard Stories

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuth, ensureGuest } =  require('../middleware/auth');
 
+const Story = require('../models/Story');
+
 // @desc   Login/Landing page
 // @route GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -13,11 +15,19 @@ router.get('/', ensureGuest, (req, res) => {
 
 // @desc   Dashboard
 // @route GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
+router.get('/dashboard', ensureAuth, async (req, res) => {
   // ensureAuth prevents those who are not logged in from reaching the Dashboard
-  res.render('dashboard', {
-    name: req.user.firstName
+
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render('dashboard', {
+    name: req.user.firstName,
+    stories
   });
+  } catch (error) {
+    console.error(error);
+    res.render('error/500');
+  }
 });
 
 module.exports = router;
